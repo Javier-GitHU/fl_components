e108a3a30c9fac5e3913992074763163294a00eb
Chavier <javiercastrorodriguez2005@gmail.com>
2024-11-05 03:48:29 +0100

    Hemos hecho el listview.separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:fl_components/srceens/listview1_screen.dart';
+import 'package:fl_components/srceens/listview2_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       //Quitar el debug
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Listview1Screen()
+      home: Listview2Screen()
     );
   }
 }
---
+++ lib/srceens/listview2_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+//fl plantila home screen
+class Listview2Screen extends StatelessWidget {
+   
+  final options = const['Targaruen', 'Stark', 'Lannister', 'Baratheon'];
+
+
+  const Listview2Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Listview Tipo2"),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          trailing: Icon(Icons.arrow_forward_ios_outlined),// LEading icono a la izquierda , trailing icono a la derecha
+          title: Text(options[index]),
+        ), //index cuenta de las veces que vamos a ir iterando 
+      separatorBuilder: (context, index) => const Divider(), //cada vez que itere añadira un divider que es una linea que separa
+      itemCount: options.length // Numero de elementos del tamño de la lista en este caso)
+    ));
+  }
+}
