2096484c5bd6ee4ea4d96ac54c39db8fdac75fc3
Chavier <javiercastrorodriguez2005@gmail.com>
2024-11-11 11:11:48 +0100

    Añadida interfaz a card sceen

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,10 +3,10 @@ class AppTheme {
    static const Color primary = Colors.green;
    static final ThemeData lightTheme = ThemeData.light().copyWith(
        primaryColor: primary,
-        // Color primario
          appBarTheme: const AppBarTheme(
          color: primary,
-         titleTextStyle: TextStyle(color: Colors.white, fontSize: 24)
+         titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+         iconTheme: IconThemeData(color: Colors.white)
       ),
       listTileTheme: const ListTileThemeData(
         iconColor: primary,
