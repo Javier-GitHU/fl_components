9459cabeb6b1a26b2cd21515c189962bd819dec9
Chavier <javiercastrorodriguez2005@gmail.com>
2024-11-29 09:07:01 +0100

    MAs cambios en el input screen

--- lib/srceens/inputs_screen.dart
+++ lib/srceens/inputs_screen.dart
@@ -10,9 +10,40 @@ class InputsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Forms: Inputs'),
       ),
-      body: const SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-         child: Text('InputsScreen'),
+      body: SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Column(
+        children: [
+          TextFormField(
+            autofocus: true, //Al entrar a la pagina el primer elemento del formulario hara que salga el teclado
+            textCapitalization: TextCapitalization.words,//Primera letra de cada palabra la hace mayuscula
+            onChanged: (value){
+              print('value: $value');
+            },
+            validator: (value){
+              if ( value!.length < 3){
+                return 'Minimo 3 caracteres';
+              }
+            },
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: InputDecoration(
+              hintText: 'Nombre y apellidos',
+              labelText: 'Nombre',
+              helperText: 'Solo letras',
+              //prefixIcon: Icon(Icons.verified_user_outlined),
+              icon: Icon(Icons.assignment_ind_outlined),
+              suffixIcon: Icon(Icons.person_2_outlined),
+              /*border: OutlineInputBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  topRight: Radius.circular(20)
+                )
+              )
+              */
+            )
+          ),
+        ],
+      ),
       ),
     );
   }
